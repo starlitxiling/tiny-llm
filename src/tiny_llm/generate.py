379037aa30d4d76e9dcece6e0a7244eded8b69e3
starlitxiling <1754165401@qq.com>
2026-07-05 01:41:00 +0800

    finished day6

--- src/tiny_llm/generate.py
+++ src/tiny_llm/generate.py
@@ -12,9 +12,33 @@ def simple_generate(
     sampler: Callable[[mx.array], mx.array] | None,
 ) -> str:
     def _step(model, y):
-        pass
-
-
+        output_logits = model(y)
+        logits = output_logits[:, -1, :]
+        if sampler is None:
+            next_token = mx.argmax(logits, axis=-1)
+        else:
+            next_token = sampler(logits)
+        return next_token
+    import pdb;pdb.set_trace()
+    tokens = tokenizer.encode(prompt)
+    tokens = mx.array(tokens)
+    tokens = mx.expand_dims(tokens, axis=0)
+    detokenizer = tokenizer._detokenizer
+    detokenizer.reset()
+    output = ""
+    while True:
+        next_token = _step(model, tokens)
+        mx.eval(next_token)          # tagged
+        if next_token.item() == tokenizer.eos_token_ids:
+            break
+        next_token = mx.expand_dims(next_token, axis=1)
+        tokens = mx.concat([tokens, next_token], axis=1)
+        detokenizer.add_token(next_token.item())
+        print(detokenizer.last_segment, end="", flush=True)
+    
+    return detokenizer.text
+    
+        
 def simple_generate_with_kv_cache(
     model: Qwen3ModelWeek2, tokenizer: TokenizerWrapper, prompt: str
 ) -> str:
